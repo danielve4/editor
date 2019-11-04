30e9ebd8a86a4ae5c0756404348d1b4f6e54bc5d
Daniel <danve4@gmail.com>
2019-11-03 18:29:40 -0600

    Enabling single word predictions

--- site.js
+++ site.js
@@ -17,6 +17,30 @@ editor.keyboard.addBinding({ key: TAB }, () => {
 
 editor.keyboard.bindings[TAB].unshift(editor.keyboard.bindings[TAB].pop());
 
-let foo = () => {
-  suggestions.trigger(['daniel', 'vega']);
+let foo = async () => {
+  const r = editor.getText(0, suggestions.getCursorPos());
+  const payload = {'text': r}
+  const url = 'http://98.253.67.62:1338/predict';
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(payload)
+  });
+
+  suggestions.trigger(await response.json());
 }
+
+
+const predictions = async (text) => {
+  const url = 'http://127.0.0.1:5000/predict';
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(text)
+  });
+  return await response.json();
+} 
